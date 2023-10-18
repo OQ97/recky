7b2a0df7128eeeff70586805e9382aba077043e0
OQ97 <146215402+OQ97@users.noreply.github.com>
2023-10-18 17:51:01 +0000

    1.1

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   erb(:homepage)
@@ -12,6 +14,14 @@ get ("/results") do
 
   discogs_key = ENV.fetch("DISCOGS_KEY")
   discogs_secret = ENV.fetch("DISCOGS_SECRET")
-  discogs_url = "https://api.discogs.com/database/search?catno=#{catalogue}&key=#{discogs_key}&secret=#{discogs_secret}"
+  discogs_url = "https://api.discogs.com/database/search?catno=#{@catalogue}&key=#{discogs_key}&secret=#{discogs_secret}"
+
+  raw_discogs_data = HTTP.get(discogs_url)
+  parsed_discogs_data = JSON.parse(raw_discogs_data)
+
+  results_array = parsed_discogs_data.fetch("results")
+  first_result_hash = results_array.at(0)
+
+  title = first_result_hash.fetch("title")
 
 end
