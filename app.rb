4cd33da7019ae0648db47d0703edf34f9b1b9da3
OQ97 <146215402+OQ97@users.noreply.github.com>
2023-10-19 03:58:35 +0000

    1.8

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ end
 
 get("/searching") do
 
-  @barcode = params.fetch("barcode").to_s.chomp
+  @catno = params.fetch("catno").to_s.chomp
 
   discogs_key = ENV.fetch("DISCOGS_KEY")
   discogs_secret = ENV.fetch("DISCOGS_SECRET")
-  @discogs_url = "https://api.discogs.com/database/search?format=vinyl&barcode=#{@barcode}&key=#{discogs_key}&secret=#{discogs_secret}"
+  @discogs_url = "https://api.discogs.com/database/search?format=vinyl&catno=#{@catno}&key=#{discogs_key}&secret=#{discogs_secret}"
 
   raw_discogs_data = HTTP.get(@discogs_url)
   parsed_discogs_data = JSON.parse(raw_discogs_data)
@@ -31,9 +31,9 @@ get("/searching") do
   
 
   if @num_results == 1
-      redirect ("/:single_release&#{@barcode}")
+      redirect ("/:single_release&#{@catno}")
   else
-      redirect ("/:multiple_releases&#{@barcode}")
+      redirect ("/:multiple_releases&#{@catno}")
   end
 
   erb(:searching)
@@ -41,11 +41,11 @@ end
 
 get ("/:multiple_releases") do
   
-  @barcode = params.fetch("multiple_releases").gsub(":multiple_releases&", "").to_i
+  @catno = params.fetch("multiple_releases").gsub(":multiple_releases&", "")
 
   discogs_key = ENV.fetch("DISCOGS_KEY")
   discogs_secret = ENV.fetch("DISCOGS_SECRET")
-  @discogs_url = "https://api.discogs.com/database/search?format=vinyl&barcode=#{@barcode}&key=#{discogs_key}&secret=#{discogs_secret}"
+  @discogs_url = "https://api.discogs.com/database/search?format=vinyl&catno=#{@catno}&key=#{discogs_key}&secret=#{discogs_secret}"
 
   raw_discogs_data = HTTP.get(@discogs_url)
   parsed_discogs_data = JSON.parse(raw_discogs_data)
@@ -59,16 +59,25 @@ get ("/:multiple_releases") do
   
   @years=[]
 
+  year = 0
+  country = 0
+
   results_array.each do |array_num|
+    begin 
     year = array_num.fetch("year").to_i
     @years.push(year)
+    rescue StandardError
+    end 
   end 
 
   @countries=[]
 
   results_array.each do |array_num|
+    begin
     country = array_num.fetch("country").to_s
     @countries.push(country)
+    rescue StandardError
+    end 
   end 
 
   erb(:multiple_releases)
